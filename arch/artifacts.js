f0bb89ff0968eb623519ab409c299b7ac083acef
ShadowGull <30505273+ShadowGull@users.noreply.github.com>
2025-04-19 04:23:25 +0930

    Fixed A2 Artifact chances
    Probabilities are now updated for fragment artifacts

--- arch/artifacts.js
+++ arch/artifacts.js
@@ -1098,10 +1098,10 @@ var Artifacts = [
     random: function (save) {
 	//(log10(1 + x) ^ 3 / 100000 (100K))%, where x is mana regen.
 	//Set to 1000 until I find a better way to do this
-      return Math.pow(Math.log10(1000 + 1),3) / 10000000;
+      return Math.pow(Math.log(1000 + 1),3) / 100000000;
     },
     required: function (value) {
-      return Math.pow(10,Math.pow(value*10000000,1/3));
+      return Math.pow(Math.E,Math.pow(value*100000000,1/3));
     },
     display: function (value) {
       return util.render.sci(value) + ' Mana per second';
@@ -1115,11 +1115,10 @@ var Artifacts = [
       return save.faction == 6 && save.reincarnation >= this.reincarnation && save.excavations >= 5000;
     },
     random: function (save) {
-	//(x ^ 3 / 5000000000 (5B))%, where x is royal exchange bonus
-      return Math.pow(util.save.re_bonus(save),3) / 500000000000;
+      return util.save.re_bonus(save) / 5000000000;
     },
     required: function (value) {
-      return Math.pow(value * 500000000000, 1/3);
+      return value * 5000000000;
     },
     display: function (value) {
       return Math.ceil(value) + '% individual Royal Exchange Bonus';
@@ -1135,10 +1134,10 @@ var Artifacts = [
     random: function (save) {
 	//(log10(1 + x) ^ 3 / 1000000 (1M))%, where x is offline bonus multiplier.
 	// Set to 1000, see above
-      return Math.pow((Math.log10(1000 + 1) - 2),3) / 50000000;
+      return Math.pow((Math.log(1000 + 1) - 2),3) / 50000000;
     },
     required: function (value) {
-      return Math.pow(10 ,Math.pow(value * 50000000,1/3) + 2);
+      return Math.pow(Math.E ,Math.pow(value * 50000000,1/3) + 2);
     },
     display: function (value) {
       return util.render.sci(value) + '% Offline Production Bonus';
@@ -1153,10 +1152,10 @@ var Artifacts = [
     },
     random: function (save) {
 	//(x / 100000000000 (100B))%, where x is assistant count.
-      return util.save.assistants(save) / 10000000000000;
+      return util.save.assistants(save) / 100000000000000;
     },
     required: function (value) {
-      return value * 10000000000000;
+      return value * 100000000000000;
     },
     display: function (value) {
       return util.render.sci(value) + ' Base Assistants';
@@ -1171,10 +1170,10 @@ var Artifacts = [
     },
     random: function (save) {
 	//(log10(1 + x) ^ 3 / 5000000 (5M))%, where x is faction coins this game.
-      return Math.pow(Math.log10(util.save.faction_coins(save) + 1),3) / 250000000;
+      return 4 * Math.pow(Math.log(util.save.faction_coins(save) + 1),3) / 5000000000;
     },
     required: function (value) {
-      return Math.pow(10,Math.pow(value*250000000,1/3));
+      return Math.pow(Math.E,Math.pow(value*5000000000 / 4.0,1/3));
     },
     display: function (value) {
       return util.render.sci(value) + ' Faction Coins found (this Era)';
@@ -1189,10 +1188,10 @@ var Artifacts = [
     },
     random: function (save) {
 	//(x / 50000000 (50M))%, where x is evil spells this R.
-      return (save.spells[1].c + save.spells[1].r + save.spells[8].c + save.spells[8].r + save.spells[15].c + save.spells[15].r + save.spells[11].c + save.spells[11].r + save.spells[4].c + save.spells[4].r) / 5000000000;
+      return 66 * (save.spells[1].c + save.spells[1].r + save.spells[8].c + save.spells[8].r + save.spells[15].c + save.spells[15].r + save.spells[11].c + save.spells[11].r + save.spells[4].c + save.spells[4].r) / 6600000000;
     },
     required: function (value) {
-      return value * 5000000000;
+      return value * 6600000000 / 66;
     },
     display: function (value) {
       return util.render.sci(value) + ' Evil Spells cast (This Reincarnation)';
@@ -1206,11 +1205,10 @@ var Artifacts = [
       return save.faction == 1 && save.reincarnation >= this.reincarnation && save.excavations >= 5000;
     },
     random: function (save) {
-	//(log10(1 + x) ^ 3) / 20000 (20K))%, where x is clicks this game.
-      return Math.pow(Math.log10(util.save.stat(save, 4) + 1),3) / 2000000;
+      return Math.pow(Math.log(util.save.stat(save, 4) + 1),3) / 2000000;
     },
     required: function (value) {
-      return Math.pow(10,Math.pow(value*2000000,1/3));
+      return Math.pow(Math.E,Math.pow(value*2000000,1/3));
     },
     display: function (value) {
       return util.render.sci(value)  + ' Clicks (This Era)';
@@ -1224,11 +1222,10 @@ var Artifacts = [
       return save.faction == 7 && save.reincarnation >= this.reincarnation && save.excavations >= 5000;
     },
     random: function (save) {
-	//(x / 20000000 (20M))%, where x is amount of buildings.
-      return util.save.total_buildings(save) / 2000000000;
+      return 2 * util.save.total_buildings(save) / 1000000000;
     },
     required: function (value) {
-      return value * 2000000000;
+      return value * 1000000000 / 2;
     },
     display: function (value) {
       return Math.ceil(value) + ' Buildings (Multipliers do NOT count)';
@@ -1243,10 +1240,10 @@ var Artifacts = [
     },
     random: function (save) {
 	//(log10(1 + x) ^ 3) / 125000 (125K))%, where x is Tax Collections this game.
-      return Math.pow(Math.log10(save.spells[18].c + 1),3) / 12500000;
+      return Math.pow(Math.log(save.spells[18].c + 1),3) / 12500000;
     },
     required: function (value) {
-      return Math.pow(10,Math.pow(value*12500000,1/3));
+      return Math.pow(Math.E,Math.pow(value*12500000,1/3));
     },
     display: function (value) {
       return util.render.sci(value) + ' Tax Collections (This Era)';
@@ -1312,10 +1309,10 @@ var Artifacts = [
       return save.reincarnation >= this.reincarnation;
     },
     random: function(save) {
-      return Math.pow(util.save.getSpentBudget(save),2) / 100000000000;
+      return Math.pow(2 * util.save.getSpentBudget(save),2) / 100000000000;
     },
 	required: function (value) {
-      return Math.pow(value * 100000000000, 1/2);
+      return 0.5 * Math.pow(value * 100000000000, 1/2);
     },
     display: function (value) {
       return Math.ceil(value) + ' Research Budget Spent';
